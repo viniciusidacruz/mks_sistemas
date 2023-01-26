d170fa444d87fa46619dd46263b00cda8db64a94
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-01-26 15:40:11 -0300

    refactor: exports components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './Logo';
+export * from './Header';
+export * from './Footer';
+export * from './MiniCart';
+export * from './Quantity';
+export * from './ProductCard';
+export * from './ProductRowCart';
