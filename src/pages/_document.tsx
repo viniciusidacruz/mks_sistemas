7a40a034712c6c1012c38eaf3264cd9bda1ca6bd
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-01-26 15:41:15 -0300

    refactor: change font for montserrat

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,7 +47,7 @@ export default class MyDocument extends Document {
           />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Roboto+Mono:wght@700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&family=Roboto+Mono:wght@700&display=swap"
             rel="stylesheet"
           />
         </Head>
