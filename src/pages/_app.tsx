e66826b534a0126ec9a390593f07b56c1eaa2b9e
Vinicius Italo <vinicius.italo@fretebras.com.br>
2023-01-26 15:41:58 -0300

    feat: implement header and footer in application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
+import { FooterComponent, HeaderComponent } from '@components/index';
+
 import { THEME_DEFAULT, ResetCSS } from '@styles/index';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={THEME_DEFAULT}>
+      <HeaderComponent />
       <ResetCSS />
       <Component {...pageProps} />
+      <FooterComponent />
     </ThemeProvider>
   );
 }
